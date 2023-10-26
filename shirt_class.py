c32587d901326b73c37025847e54249252d5ab40
login <mkanderson771@gmail.com>
2023-10-26 17:09:49 +0300

    checking how referencing in oop takes place

--- shirt_class.py
+++ shirt_class.py
@@ -18,16 +18,21 @@ class Shirt:
 shirt_one = Shirt("red", "S", "short sleeve", 15)
 shirt_two = Shirt("orange", "M", "short sleeve", 25)
 shirt_three = Shirt("Purple", "XL", "short sleeve", 10)
-
+shirt_four = shirt_one
+print(shirt_one)
+print(shirt_four)
+shirt_one_price = shirt_four.price
 # Use change_price method to change price of shirt_one
 shirt_one.change_price(5)
 print("The new price of shirt one is: {}".format(shirt_one.price))
+# Check to see if attribute references changes once its value is changed
+print(shirt_one_price)
 
 # Calculate the combined discounts using the rates given
-print(f"The total discount is: {shirt_one.discount(.14)+shirt_two.discount(.06)+shirt_three.discount(.05)}")
+print(f"The total discount is: {shirt_one.discount(.14)+shirt_two.discount(.06)+shirt_three.discount(.05)+shirt_four.discount(.10)}")
 
 # print shirt attributes
-shirts = [shirt_one, shirt_two, shirt_three]
+shirts = [shirt_one, shirt_two, shirt_three, shirt_four]
 for _ in shirts:
     print(f"Color: {_.color}\t size: {_.size}\t style: {_.style}\t Price after discount at 10%: {_.price - (_.discount(.10))}")
 
